d66db5294712aab0f3b5965808bae624676a6f48
Scott Obara <scott@rubbscreations.com>
2021-02-21 14:27:39 +1100

    FileManager-Decoder.swift

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,27 +7,30 @@
 
 import SwiftUI
 
-struct User: Identifiable, Comparable {
-    let id = UUID()
-    let firstName: String
-    let lastName: String
-    
-    static func < (lhs: User, rhs: User) -> Bool {
-        lhs.lastName < rhs.lastName
-    }
-}
-
 struct ContentView: View {
-    let users = [
-        User(firstName: "Arnold", lastName: "Rimmer"),
-        User(firstName: "Kristine", lastName: "Kochanski"),
-        User(firstName: "David", lastName: "Lister"),
-    ].sorted()
-
+    
+    func getDocumentsDirectory() -> URL {
+        // find all possible documents directories for this user
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        // just send back the first one, which ought to be the only one
+        return paths[0]
+    }
+    
     var body: some View {
-        List(users) { user in
-            Text("\(user.lastName), \(user.firstName)")
-        }
+        Text("Hello World")
+            .onTapGesture {
+                let str = "Test Message"
+                let url = self.getDocumentsDirectory().appendingPathComponent("message.txt")
+                
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
     }
 }
 
