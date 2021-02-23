43d251813bd21bea15bacb4effe8ede8e877930c
Scott Obara <scott@rubbscreations.com>
2021-02-23 20:30:58 +1100

    Day 69 End

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,13 +5,45 @@
 //  Created by Scott Obara on 21/2/21.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isUnlocked = false
     
     var body: some View {
-        MapView()
-            .edgesIgnoringSafeArea(.all)
+        VStack {
+            if self.isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                DispatchQueue.main.async {
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        // there was a problem
+                    }
+                }
+            }
+        } else {
+            // no biometrics
+        }
     }
 }
 
